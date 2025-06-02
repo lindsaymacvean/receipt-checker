c79d268e32ed6c4048be889c9bb4e27ea4092763
lindsay macvean <lindsaymacvean@gmail.com>
2025-06-02 20:13:59 +0100

    Restore CloudFormation checks in pre_commit_cfn.sh by uncommenting relevant commands for validation and linting of the template.

--- backend/scripts/pre_commit_cfn.sh
+++ backend/scripts/pre_commit_cfn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# echo "Running CloudFormation checks..."
-# cfn-lint backend/template.yaml
-# aws cloudformation validate-template --region eu-west-1 --template-body file://backend/template.yaml
-# echo "Done!"
+echo "Running CloudFormation checks..."
+cfn-lint backend/template.yaml
+aws cloudformation validate-template --region eu-west-1 --template-body file://backend/template.yaml
+echo "Done!"
